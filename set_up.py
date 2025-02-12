35aacb9636988d29bff0ed5e0a71bb7dbdb2706b
m_duration lab <a.m.van.harmelen@vu.nl>
2025-02-12 15:20:07 +0100

    update dir

--- set_up.py
+++ set_up.py
@@ -34,7 +34,7 @@ def get_monitor_and_dir(testing: bool):
             "distance": 70,  # in cm
         }
 
-        directory = r"C:\Users\Anna_vidi\Desktop\microsaccade_data"
+        directory = r"C:\Users\m_bias_duration\Desktop\Duration data"
 
     return monitor, directory
 
